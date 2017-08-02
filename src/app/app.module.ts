5092fc4c2d0ef935b9a425cab5d721ce9d69c0d8
Siddhi Air <siddhiair8@gmail.com>
2017-08-02 15:27:58 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { SubscriptionComponent } from './subscription/subscription.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+
+const appRoutes:Routes=[
+  {path:'', component:HomeComponent},
+  {path:'contact', component: ContactComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    SubscriptionComponent,
+    FooterComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/assets/js/custom.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$("#opneSearch").click(function(){
+		console.log("clicked");
+		$("#searchfield").animate({width:"250px"});
+	});
+});
